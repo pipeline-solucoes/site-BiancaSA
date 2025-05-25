24432e00002a4ab75ed62c543270569ec07fe6dd
Juliana Quaresma <julianaqribeiro@hotmail.com>
2025-05-25 11:41:21 -0300

    Ajuste do icone do card de servico e do efeito da secao formulario

--- src/app/secao-home/SecaoFormulario.tsx
+++ src/app/secao-home/SecaoFormulario.tsx
@@ -24,50 +24,52 @@ const SecaoFormulario: React.FC<SecaoFormularioProps> = ({isXs}) => {
     
     <Section section_id="formulario" background_color={theme.palette.custom?.backgroundSectionAlternative}>
 
-      <ContentSectionWrapStyled>
-        
-        <SectionTextColumnStyle textColor={colorText}>
-          <TitleSection 
-            title_text="Fale com a Bianca" 
-            subtitle_text="Estou aqui para ajudar">          
-          </TitleSection> 
-          <p>Meu compromisso é oferecer um atendimento personalizado e humanizado, tanto no conforto do seu lar quanto no acolhimento do nosso consultório. Acredito que um ambiente de confiança e empatia é fundamental para um tratamento eficaz.</p>          
-          <ContainerList>
-            <IconCheckText typeCheck="VerifiedIcon" marginLeft="0px" iconColor={colorIconCheck} textColor={colorText}><b>Agendamento de Consultas:</b> Marque uma consulta no horário e local que melhor se adequem à sua rotina.</IconCheckText>
-            <IconCheckText typeCheck="VerifiedIcon" marginLeft="0px" iconColor={colorIconCheck} textColor={colorText}><b>Esclarecimento de Dúvidas:</b> Estou a disposição para responder às suas perguntas e oferecer orientações claras e precisas.</IconCheckText>
-            <IconCheckText typeCheck="VerifiedIcon" marginLeft="0px" iconColor={colorIconCheck} textColor={colorText}><b>Suporte e Acompanhamento:</b> Precisando de um pouco de apoio extra? Estou aqui para ouvir e ajudar.</IconCheckText>                    
-          </ContainerList>                    
-        </SectionTextColumnStyle>
-        
-        <ContainerAnimatedScroll>
-          <ShadowCard sombraClara={sombraClara} sombraEscura={sombraEscura} width={widthForm} height="fit-content" border_radius="20px" padding="0px">            
-            <Box display="flex" flexDirection='column' justifyContent="center" gap="24px" marginTop='8px' flex={1} 
-              sx={{ backgroundColor: theme.palette.custom.backgroundSectionAlternative, padding: "24px", borderRadius: '10px' }} >
-                {/* reCAPTCHA: 6LfNNjQrAAAAAJdYsNsPL8RFXM6krfURGLX9Ze98  */}
-                <Form token={tokenFormulario}
-                  background_color={theme.palette.custom.backgroundSectionMain} 
-                  color={colorText} 
-                  border_radius="10px" 
-                  background_color_button={theme.palette.primary.main} 
-                  border_radius_button={theme.palette.custom.borderRadiusButtonCTA}
-                  color_button={theme.palette.primary.contrastText} 
-                  text_button="Enviar mensagem" 
-                  message_sucess="Agradeço pelo seu contato. Em breve, responderei com atenção. Até logo!"
-                  message_erro="Houve um problema ao enviar sua mensagem. Por favor, verifique sua conexão e tente novamente mais tarde. 
-                  Caso o erro persista, saiba que você também pode me contatar pelos outros canais disponíveis."
-                  color_message_erro={theme.palette.error.main}
-                  color_message_sucess={theme.palette.success.main}
-                />
-                <ContainerList>
-                  <Telefone color={theme.palette.primary.main}></Telefone>
-                  <Email color={theme.palette.primary.main}></Email>
-                  <SocialMedia position="faleconosco"></SocialMedia>
-                </ContainerList>                        
-            </Box>
-          </ShadowCard>
-        </ContainerAnimatedScroll> 
+      <ContainerAnimatedScroll>
 
-      </ContentSectionWrapStyled>
+        <ContentSectionWrapStyled>
+          
+            <SectionTextColumnStyle textColor={colorText}>
+              <TitleSection 
+                title_text="Fale com a Bianca" 
+                subtitle_text="Estou aqui para ajudar">          
+              </TitleSection> 
+              <p>Meu compromisso é oferecer um atendimento personalizado e humanizado, tanto no conforto do seu lar quanto no acolhimento do nosso consultório. Acredito que um ambiente de confiança e empatia é fundamental para um tratamento eficaz.</p>          
+              <ContainerList>
+                <IconCheckText typeCheck="VerifiedIcon" marginLeft="0px" iconColor={colorIconCheck} textColor={colorText}><b>Agendamento de Consultas:</b> Marque uma consulta no horário e local que melhor se adequem à sua rotina.</IconCheckText>
+                <IconCheckText typeCheck="VerifiedIcon" marginLeft="0px" iconColor={colorIconCheck} textColor={colorText}><b>Esclarecimento de Dúvidas:</b> Estou a disposição para responder às suas perguntas e oferecer orientações claras e precisas.</IconCheckText>
+                <IconCheckText typeCheck="VerifiedIcon" marginLeft="0px" iconColor={colorIconCheck} textColor={colorText}><b>Suporte e Acompanhamento:</b> Precisando de um pouco de apoio extra? Estou aqui para ouvir e ajudar.</IconCheckText>                    
+              </ContainerList>                    
+            </SectionTextColumnStyle>
+                  
+            <ShadowCard sombraClara={sombraClara} sombraEscura={sombraEscura} width={widthForm} height="fit-content" border_radius="20px" padding="0px">            
+              <Box display="flex" flexDirection='column' justifyContent="center" gap="24px" marginTop='8px' flex={1} 
+                sx={{ backgroundColor: theme.palette.custom.backgroundSectionAlternative, padding: "24px", borderRadius: '10px' }} >
+                  {/* reCAPTCHA: 6LfNNjQrAAAAAJdYsNsPL8RFXM6krfURGLX9Ze98  */}
+                  <Form token={tokenFormulario}
+                    background_color={theme.palette.custom.backgroundSectionMain} 
+                    color={colorText} 
+                    border_radius="10px" 
+                    background_color_button={theme.palette.primary.main} 
+                    border_radius_button={theme.palette.custom.borderRadiusButtonCTA}
+                    color_button={theme.palette.primary.contrastText} 
+                    text_button="Enviar mensagem" 
+                    message_sucess="Agradeço pelo seu contato. Em breve, responderei com atenção. Até logo!"
+                    message_erro="Houve um problema ao enviar sua mensagem. Por favor, verifique sua conexão e tente novamente mais tarde. 
+                    Caso o erro persista, saiba que você também pode me contatar pelos outros canais disponíveis."
+                    color_message_erro={theme.palette.error.main}
+                    color_message_sucess={theme.palette.success.main}
+                  />
+                  <ContainerList>
+                    <Telefone color={theme.palette.primary.main}></Telefone>
+                    <Email color={theme.palette.primary.main}></Email>
+                    <SocialMedia position="faleconosco"></SocialMedia>
+                  </ContainerList>                        
+              </Box>
+            </ShadowCard>
+        
+        </ContentSectionWrapStyled>
+
+       </ContainerAnimatedScroll> 
             
     </Section>            
   );  
--- src/app/secao-home/SecaoServicos.tsx
+++ src/app/secao-home/SecaoServicos.tsx
@@ -3,42 +3,23 @@
 import { AnimatedCardData, AnimatedCardGroup, ContainerAnimatedScroll, ContentSectionWrapStyled, NavigationButton, Section } from "pipesolcomponents";
 import { Typography, useTheme } from "@mui/material";
 import TitleSectionCenter from "../components/TitleSectionCenter";
-import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown';
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp';
 import { sombraClara, sombraEscura } from "@/constants";
-
-// interface IServico {
-//   url: string;
-//   title: string;
-//   description: string;
-// }
-
-// const servicos : IServico[] = [
-//     {url: '/servicos/cuidadospaliativos', title: 'Cuidados Paliativos', description: 'Assistência focada no alívio da dor e promoção de conforto físico e emocional para pacientes com doenças crônicas ou em estágio avançado.'},
-//     {url: '#sobre', title: 'Fotobiomodulação', description: 'Terapia com luz de baixa intensidade que estimula a regeneração celular, alivia dores e inflamações, acelerando a cicatrização.'},
-//     {url: '#sobre', title: 'Perícia Judicial', description: 'Avaliação técnica especializada para processos judiciais, com emissão de laudos precisos e embasados na prática fisioterapêutica.'},
-//     {url: '#sobre', title: 'Pós Operatório', description: 'Acelera a recuperação após cirurgias, aliviando dores, reduzindo inchaços e restaurando movimentos e funções do corpo.'},
-//     {url: '#sobre', title: 'Pré Habilitação', description: 'Preparação física e funcional antes de cirurgias para melhorar os resultados e reduzir o tempo de recuperação no pós-operatório.'},
-//     {url: '#sobre', title: 'Reabilitação', description: 'Foca na recuperação funcional após traumas ou doenças, promovendo autonomia, qualidade de vida e prevenção de novas lesões.'},
-//     {url: '#sobre', title: 'Reabilitação Oncológica', description: 'Cuidados personalizados para pacientes com câncer, promovendo mobilidade, controle da dor e qualidade de vida durante e após o tratamento.'},
-//     {url: '#sobre', title: 'Reabilitação Geriátrica', description: 'Atendimento voltado à pessoa idosa, com foco na prevenção de quedas, manutenção da autonomia e melhora da mobilidade e funcionalidade.'},
-//     {url: '#sobre', title: 'Tratamento de Disfunções Linfáticas', description: 'Abordagem para controle de edemas (como o linfedema), por meio de drenagem linfática, exercícios e cuidados específicos.'},
-//     {url: '#sobre', title: 'Tratamento de Disfunções Vasculares', description: 'Intervenções que visam melhorar a circulação sanguínea, aliviar sintomas de insuficiência venosa e prevenir complicações.'},
-//     {url: '#sobre', title: 'Tratamento da Neuropatia Periférica', description: 'Terapias para alívio da dor, formigamento e fraqueza causados por lesões nos nervos periféricos, com foco em funcionalidade e bem-estar.'},    
-//   ];        
 
 const cards: AnimatedCardData[] = [
 
-  {id: 0, icon: ExpandCircleDownIcon, title: 'Cuidados Paliativos', description: 'Assistência focada no alívio da dor e promoção de conforto físico e emocional para pacientes com doenças crônicas ou em estágio avançado.'},
-  {id: 1, icon: ExpandCircleDownIcon, title: 'Fotobiomodulação', description: 'Terapia com luz de baixa intensidade que estimula a regeneração celular, alivia dores e inflamações, acelerando a cicatrização.'},
-  {id: 2, icon: ExpandCircleDownIcon, title: 'Perícia Judicial', description: 'Avaliação técnica especializada para processos judiciais, com emissão de laudos precisos e embasados na prática fisioterapêutica.'},
-  {id: 3, icon: ExpandCircleDownIcon, title: 'Pós Operatório', description: 'Acelera a recuperação após cirurgias, aliviando dores, reduzindo inchaços e restaurando movimentos e funções do corpo.'},
-  {id: 4, icon: ExpandCircleDownIcon, title: 'Pré Habilitação', description: 'Preparação física e funcional antes de cirurgias para melhorar os resultados e reduzir o tempo de recuperação no pós-operatório.' },
-  {id: 5, icon: ExpandCircleDownIcon, title: 'Reabilitação' , description: 'Foca na recuperação funcional após traumas ou doenças, promovendo autonomia, qualidade de vida e prevenção de novas lesões.'},
-  {id: 6, icon: ExpandCircleDownIcon, title: 'Reabilitação Geriátrica', description: 'Atendimento voltado à pessoa idosa, com foco na prevenção de quedas, manutenção da autonomia e melhora da mobilidade e funcionalidade.' },
-  {id: 7, icon: ExpandCircleDownIcon, title: 'Reabilitação Oncológica', description: 'Cuidados personalizados para pacientes com câncer, promovendo mobilidade, controle da dor e qualidade de vida durante e após o tratamento.'},
-  {id: 8, icon: ExpandCircleDownIcon, title: 'Tratamento de Disfunções Linfáticas', description: 'Abordagem para controle de edemas (como o linfedema), por meio de drenagem linfática, exercícios e cuidados específicos.'},
-  {id: 9, icon: ExpandCircleDownIcon, title: 'Tratamento de Disfunções Vasculares', description: 'Intervenções que visam melhorar a circulação sanguínea, aliviar sintomas de insuficiência venosa e prevenir complicações.'},
-  {id: 10, icon: ExpandCircleDownIcon, title: 'Tratamento da Neuropatia Periférica', description: 'Terapias para alívio da dor, formigamento e fraqueza causados por lesões nos nervos periféricos, com foco em funcionalidade e bem-estar.'},  
+  {id: 0, title: 'Cuidados Paliativos', description: 'Assistência focada no alívio da dor e promoção de conforto físico e emocional para pacientes com doenças crônicas ou em estágio avançado.'},
+  {id: 1, title: 'Fotobiomodulação', description: 'Terapia com luz de baixa intensidade que estimula a regeneração celular, alivia dores e inflamações, acelerando a cicatrização.'},
+  {id: 2, title: 'Perícia Judicial', description: 'Avaliação técnica especializada para processos judiciais, com emissão de laudos precisos e embasados na prática fisioterapêutica.'},
+  {id: 3, title: 'Pós Operatório', description: 'Acelera a recuperação após cirurgias, aliviando dores, reduzindo inchaços e restaurando movimentos e funções do corpo.'},
+  {id: 4, title: 'Pré Habilitação', description: 'Preparação física e funcional antes de cirurgias para melhorar os resultados e reduzir o tempo de recuperação no pós-operatório.' },
+  {id: 5, title: 'Reabilitação' , description: 'Foca na recuperação funcional após traumas ou doenças, promovendo autonomia, qualidade de vida e prevenção de novas lesões.'},
+  {id: 6, title: 'Reabilitação Geriátrica', description: 'Atendimento voltado à pessoa idosa, com foco na prevenção de quedas, manutenção da autonomia e melhora da mobilidade e funcionalidade.' },
+  {id: 7, title: 'Reabilitação Oncológica', description: 'Cuidados personalizados para pacientes com câncer, promovendo mobilidade, controle da dor e qualidade de vida durante e após o tratamento.'},
+  {id: 8, title: 'Tratamento de Disfunções Linfáticas', description: 'Abordagem para controle de edemas (como o linfedema), por meio de drenagem linfática, exercícios e cuidados específicos.'},
+  {id: 9, title: 'Tratamento de Disfunções Vasculares', description: 'Intervenções que visam melhorar a circulação sanguínea, aliviar sintomas de insuficiência venosa e prevenir complicações.'},
+  {id: 10, title: 'Tratamento da Neuropatia Periférica', description: 'Terapias para alívio da dor, formigamento e fraqueza causados por lesões nos nervos periféricos, com foco em funcionalidade e bem-estar.'},  
 ]
 
 interface SecaoServicosProps{
@@ -63,11 +44,14 @@ const SecaoServicos: React.FC<SecaoServicosProps> = ({isXs, isSm}) => {
         <ContentSectionWrapStyled marginTop="40px" marginBottom="40px">
           
           <ContainerAnimatedScroll>          
-            <AnimatedCardGroup cards={cards} 
+            <AnimatedCardGroup 
+              cards={cards} 
+              icon={ArrowCircleDownIcon}
+              icon_expanded={ArrowCircleUpIcon} 
               background_color='transparent' 
-              background_color_hover={theme.palette.primary.main} 
+              background_color_expanded={theme.palette.primary.main} 
               color={theme.palette.primary.main} 
-              color_hover={theme.palette.custom?.backgroundSectionAlternative}
+              color_expanded={theme.palette.custom?.backgroundSectionAlternative}
               border_radius="20px"
               width_card={width_card}
               min_height_card="30px"
