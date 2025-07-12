120cbd557887155544fdde4d3df8b5175fcad123
Juliana Quaresma <julianaqribeiro@hotmail.com>
2025-07-12 10:53:05 -0300

    Ajuste na foto do banner e do favicon

--- src/app/components/banner/HeaderBarInCustom.tsx
+++ src/app/components/banner/HeaderBarInCustom.tsx
@@ -14,7 +14,7 @@ const HeaderBarInCustom: React.FC<HeaderBarInCustomProps> = ({ isXs, isSm, items
     const height = (isXs) ? '850px' : (isSm ? '950px' : '800px') ;
 
     return (
-       <HeaderBarIn background_color='transparent' border_radius="40px" top_bar={10}
+      <HeaderBarIn background_color='transparent' border_radius="40px" top_bar={10}
             height_xs={height} height_sm={height} height_md={height} height_lg={height} height_xl={height}                
             renderBar={() => <BarraFerramentasCustom isXs={isXs} isSm={isSm} itens_menu={itemsMenu}></BarraFerramentasCustom>}
             renderBanner={() => <Banner isXs={isXs} isSm={isSm}></Banner>}>        
